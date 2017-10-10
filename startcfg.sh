590df4c1efddc8ea6346d29ac379df0b6432f326
greencrab <greencrab@gmail.com>
2017-10-09 20:36:43 -0700

    script from blog to init bare repo for cfg

---
+++ startcfg.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Initialize alias to store config, details from this post
+# https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/
+#
+
+git clone --bare https://github.com/greencrab/dotfiles.git $HOME/dotfiles
+function config {
+   /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+
